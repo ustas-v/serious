ea79b516b30f80740caf1697d3ea5884f085d3f4
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-08 02:07:44 +0600

    Add serious/i18n

---
+++ lib/serious/i18n.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require 'i18n'
+
+I18n.load_path << Dir[File.expand_path('../locale/**/*.yml', __FILE__)]
+
+if Serious.root
+  I18n.load_path << Dir[File.expand_path('locales/**/*.yml', Serious.root)]
+end
