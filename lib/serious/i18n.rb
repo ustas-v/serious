217ea21fa6d23ac3881e5da2f39e409b364e4f1d
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-10 00:08:13 +0600

    Reload I18n to process the local traslations

--- lib/serious/i18n.rb
+++ lib/serious/i18n.rb
@@ -7,3 +7,5 @@ I18n.load_path << Dir[File.expand_path('../locale/**/*.yml', __FILE__)]
 if Serious.root
   I18n.load_path << Dir[File.expand_path('locales/**/*.yml', Serious.root)]
 end
+
+I18n.reload!
